f0305f8c19dae279c5798d8b22e6f1d132a15d09
gowtham <gowtham@valuestreambiz.com>
2026-03-09 12:28:42 +0530

    added category master for the data entry categories in the Expense page

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Donation, ExpenseRecord, PaymentRecord, PassbookEntry
+from .models import Donation, ExpenseCategory, ExpenseRecord, PaymentRecord, PassbookEntry
 
 
 @admin.register(PaymentRecord)
@@ -17,6 +17,13 @@ class ExpenseRecordAdmin(admin.ModelAdmin):
     list_display = ("id", "transaction_no", "category", "amount", "transaction_date", "created_by", "created_at")
     list_filter = ("category", "transaction_date")
     search_fields = ("transaction_no", "category", "comments", "remarks", "created_by__name", "created_by__phone_number")
+
+
+@admin.register(ExpenseCategory)
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "group_key", "display_order", "is_active", "updated_at")
+    list_filter = ("group_key", "is_active")
+    search_fields = ("name",)
 
 
 @admin.register(PassbookEntry)
